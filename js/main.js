f077c9fafa321cc652b254c3e2c7a44c1b31c5e0
aparnell0130 <70460766+aparnell0130@users.noreply.github.com>
2020-11-14 10:25:24 -0800

    testing

--- js/main.js
+++ js/main.js
@@ -47,46 +47,65 @@ function getClosestDate(launchDates, targetDate) {
 
 launchLibrary();
 
-var apiKey = 'Kh6At4nhdqEhFXjs1OINKDv99FrMOgT6XVHJjJCt'
-var date = '05-18'
-var year = Math.floor(Math.random() * (moment().format('yyyy') - 1996) + 1996)
-var queryURL = `https://api.nasa.gov/planetary/apod?date=${year}-${date}&api_key=${apiKey} `;
-console.log(year)
-$.ajax({
-    url: queryURL,
-    method: 'GET'
-}).then(function (response) {
-    console.log(response);
-    var nasaDate = response.date
-    var imgDesc = response.explanation
-    var imgTitle = response.title
-    var nasaImg = response.hdurl
-    var nasaDiv = $('<div>')
-    var divDate = $('<h3>').text(nasaDate)
-    var divTitle = $('<h2>').text(imgTitle)
-    var divImg = $('<img>').attr('src', nasaImg).css({ 'width': '300px', 'height': 'auto' })
-    var description = $('<p>').text(imgDesc)
-    $('body').append(nasaDiv)
-    nasaDiv.append(divTitle, divDate, divImg, description)
-    // var apiKey = 'Kh6At4nhdqEhFXjs1OINKDv99FrMOgT6XVHJjJCt'
-    // var date2 = '2015-05-18'
-    // var queryURL2 = `https://api.nasa.gov/planetary/apod?date=${date2}&api_key=${apiKey}`;
-    // $.ajax({
-    //     url: queryURL2,
-    //     method: 'GET'
-    // }).then(function (response2) {
-    //     console.log(response2);
-    //     var nasaDate = response2.date
-    //     var imgDesc = response2.explanation
-    //     var imgTitle = response2.title
-    //     var nasaImg = response2.hdurl
-    //     var nasaDiv = $('<div>')
-    //     var divDate = $('<h3>').text(nasaDate)
-    //     var divTitle = $('<h2>').text(imgTitle)
-    //     var divImg = $('<img>').attr('src', nasaImg).css({ 'width': '300px', 'height': 'auto' })
-    //     var description = $('<p>').text(imgDesc)
-    //     $('body').append(nasaDiv)
-    //     nasaDiv.append(divTitle, divDate, divImg, description)
-    // })
+var year = []
+$('#searchBtn').click(function (event) {
+    event.preventDefault()
+    year = []
+    date = $('#searchDate').val()
+
+    randomYear()
+    getNasa()
+
 })
 
+
+function randomYear() {
+
+    for (let i = 0; i < 2; i++) {
+        var randomYr = Math.floor(Math.random() * (moment().format('yyyy') - 1995) + 1995)
+        year.push(randomYr)
+    }
+    console.log(year)
+}
+function getNasa() {
+    var apiKey = 'Kh6At4nhdqEhFXjs1OINKDv99FrMOgT6XVHJjJCt'
+    var date = $('#searchDate').val()
+    var queryURL = `https://api.nasa.gov/planetary/apod?date=${year[0]}-${date}&api_key=${apiKey} `;
+
+    $.ajax({
+        url: queryURL,
+        method: 'GET'
+    }).then(function (response) {
+        // console.log(response);
+        var nasaDate = moment(response.date).format('dddd, MMMM Do YYYY')
+        var imgDesc = response.explanation
+        var imgTitle = response.title
+        var nasaImg = response.hdurl
+        var nasaDiv = $('<div>')
+        var divDate = $('<h3>').text(nasaDate)
+        var divTitle = $('<h2>').text(imgTitle)
+        var divImg = $('<img>').attr('src', nasaImg).css({ 'width': '300px', 'height': 'auto' })
+        var description = $('<p>').text(imgDesc)
+        $('body').append(nasaDiv)
+        nasaDiv.append(divTitle, divDate, divImg, description)
+        var queryURL2 = `https://api.nasa.gov/planetary/apod?date=${year[1]}-${date}&api_key=${apiKey}`;
+        $.ajax({
+            url: queryURL2,
+            method: 'GET'
+        }).then(function (response2) {
+            // console.log(response2);
+            var nasaDate2 = moment(response2.date).format('dddd, MMMM Do YYYY')
+            var imgDesc2 = response2.explanation
+            var imgTitle2 = response2.title
+            var nasaImg2 = response2.hdurl
+            var nasaDiv2 = $('<div>')
+            var divDate2 = $('<h3>').text(nasaDate2)
+            var divTitle2 = $('<h2>').text(imgTitle2)
+            var divImg2 = $('<img>').attr('src', nasaImg2).css({ 'width': '300px', 'height': 'auto' })
+            var description2 = $('<p>').text(imgDesc2)
+            $('body').append(nasaDiv2)
+            nasaDiv.append(divTitle2, divDate2, divImg2, description2)
+        })
+    })
+
+}
