e9cf22815f56ed0378c66095e970fcc08f7edb36
aparnell0130 <70460766+aparnell0130@users.noreply.github.com>
2020-11-14 11:44:17 -0800

    fixed random year function, added calls to get more data

--- js/main.js
+++ js/main.js
@@ -61,9 +61,11 @@ $('#searchBtn').click(function (event) {
 
 function randomYear() {
 
-    for (let i = 0; i < 2; i++) {
+    while (year.length < 4) {
         var randomYr = Math.floor(Math.random() * (moment().format('yyyy') - 1995) + 1995)
-        year.push(randomYr)
+        if (!(year.includes(randomYr))) {
+            year.push(randomYr)
+        }
     }
     console.log(year)
 }
@@ -80,7 +82,7 @@ function getNasa() {
         var nasaDate = moment(response.date).format('dddd, MMMM Do YYYY')
         var imgDesc = response.explanation
         var imgTitle = response.title
-        var nasaImg = response.hdurl
+        var nasaImg = response.url
         var nasaDiv = $('<div>')
         var divDate = $('<h3>').text(nasaDate)
         var divTitle = $('<h2>').text(imgTitle)
@@ -93,11 +95,11 @@ function getNasa() {
             url: queryURL2,
             method: 'GET'
         }).then(function (response2) {
-            console.log(response2);
+            // console.log(response2);
             var nasaDate2 = moment(response2.date).format('dddd, MMMM Do YYYY')
             var imgDesc2 = response2.explanation
             var imgTitle2 = response2.title
-            var nasaImg2 = response2.hdurl
+            var nasaImg2 = response2.url
             var nasaDiv2 = $('<div>')
             var divDate2 = $('<h3>').text(nasaDate2)
             var divTitle2 = $('<h2>').text(imgTitle2)
@@ -105,6 +107,42 @@ function getNasa() {
             var description2 = $('<p>').text(imgDesc2)
             $('body').append(nasaDiv2)
             nasaDiv.append(divTitle2, divDate2, divImg2, description2)
+            var queryURL3 = `https://api.nasa.gov/planetary/apod?date=${year[2]}-${date}&api_key=${apiKey}`;
+            $.ajax({
+                url: queryURL3,
+                method: 'GET'
+            }).then(function (response3) {
+                // console.log(response3);
+                var nasaDate3 = moment(response3.date).format('dddd, MMMM Do YYYY')
+                var imgDesc3 = response3.explanation
+                var imgTitle3 = response3.title
+                var nasaImg3 = response3.url
+                var nasaDiv3 = $('<div>')
+                var divDate3 = $('<h3>').text(nasaDate3)
+                var divTitle3 = $('<h2>').text(imgTitle3)
+                var divImg3 = $('<img>').attr('src', nasaImg3).css({ 'width': '300px', 'height': 'auto' })
+                var description3 = $('<p>').text(imgDesc3)
+                $('body').append(nasaDiv3)
+                nasaDiv.append(divTitle3, divDate3, divImg3, description3)
+                var queryURL4 = `https://api.nasa.gov/planetary/apod?date=${year[3]}-${date}&api_key=${apiKey}`;
+                $.ajax({
+                    url: queryURL4,
+                    method: 'GET'
+                }).then(function (response4) {
+                    // console.log(response4);
+                    var nasaDate4 = moment(response4.date).format('dddd, MMMM Do YYYY')
+                    var imgDesc4 = response4.explanation
+                    var imgTitle4 = response4.title
+                    var nasaImg4 = response4.url
+                    var nasaDiv4 = $('<div>')
+                    var divDate4 = $('<h3>').text(nasaDate4)
+                    var divTitle4 = $('<h2>').text(imgTitle4)
+                    var divImg4 = $('<img>').attr('src', nasaImg4).css({ 'width': '300px', 'height': 'auto' })
+                    var description4 = $('<p>').text(imgDesc4)
+                    $('body').append(nasaDiv4)
+                    nasaDiv.append(divTitle4, divDate4, divImg4, description4)
+                })
+            })
         })
     })
 
